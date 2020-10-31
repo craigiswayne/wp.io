27057352f78bd1ccf3ab2daf2fc84ae394912bd7
Craig Wayne <craigiswayne@gmail.com>
2020-10-31 11:33:18 +0200

    More Custom Login Page things

--- wp-content/mu-plugins/custom-login-logo.php
+++ wp-content/mu-plugins/custom-login-logo.php
@@ -7,15 +7,14 @@ body{
 }
 .login h1 a{
     background-image: url(http://mysmartstaff.co.za/wp-content/uploads/2018/12/smartstaff_logo_header3.png);
-    margin-bottom: 0;
     background-size: contain;
     background-position: center;
     width: 220px;
     height: 60px;
 }
 .wp-core-ui .button-primary:hover {
-    background-color: #f77b00;;
-    border-color: #f77b00;;
+    background-color: #de6e00;;
+    border-color: #de6e00;;
 }
 
 .wp-core-ui .button-primary {
@@ -23,12 +22,18 @@ body{
     border-color: #f77b00;
 }
 
-.login #nav a, .login #backtoblog a {
+.login #nav a {
     color: white;
+    text-align: center;
+    display: block;
 }
 
 .dashicons, .login #nav a:hover, .login #backtoblog a:hover, .login h1 a:hover {
     color: #f77b00;
+}
+
+.login .message, .login .success, .login #login_error{
+	border-left-color: #f77b00;
 }
 
 div#mask {
@@ -42,7 +47,16 @@ div#mask {
     width: 100%;
     height: 100%;
     z-index: -1;
-    -webkit-filter: blur(4px) grayscale(70%);
+    -webkit-filter: grayscale(80%);
+    filter: grayscale(80%);
+}
+
+input[type="text"]:focus, input[type="password"]:focus, input[type="color"]:focus, input[type="date"]:focus, input[type="datetime"]:focus, input[type="datetime-local"]:focus, input[type="email"]:focus, input[type="month"]:focus, input[type="number"]:focus, input[type="search"]:focus, input[type="tel"]:focus, input[type="time"]:focus, input[type="url"]:focus, input[type="week"]:focus, input[type="checkbox"]:focus, input[type="radio"]:focus, select:focus, textarea:focus {
+    border-color: #f67b00;
+    box-shadow: 0 0 0 1px #f67b00;
+}
+#backtoblog{
+	display: none;
 }
 </style>';
 });
